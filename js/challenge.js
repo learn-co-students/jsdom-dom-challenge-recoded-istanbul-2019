6b63a74805b334ed45cc175ee4cebb0bc80c1419
MohammedAlabd <mu96abd@gmail.com>
2020-01-23 16:44:33 +0300

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,51 @@
+let counter = document.getElementById("counter")
+let inc = 1
+let pls = document.querySelector("#plus")
+let minus = document.querySelector("#minus")
+let heart = document.querySelector("#heart")
+let pause = document.querySelector("#pause")
+let commint = document.querySelector("#submit")
+function incress(){
+    let a =  parseInt(counter.innerText)
+    a += inc;
+    counter.innerText = a
+}
+
+setInterval(incress,1000)
+pls.addEventListener("click",incress);
+
+minus.addEventListener("click",function() {
+    let b = parseInt(counter.innerText)
+    b -= inc;
+    counter.innerText = b
+})
+
+heart.addEventListener("click",function(){
+    let a = document.createElement("li");
+    a.innerHTML = `${counter.textContent} has been liked 1 time
+    `
+    document.querySelector(".likes").appendChild(a)
+})
+
+commint.addEventListener("click",function(e){
+    e.preventDefault()
+    let a = document.createElement("p")
+    a.innerHTML = document.querySelector("#comment-input").value;
+    document.querySelector("#list").appendChild(a);
+})
+
+pause.addEventListener("click",function(){
+    if (pls.disabled == false){
+        pls.disabled = true;
+        minus.disabled = true;
+        heart.disabled = true;
+        commint.disabled = true;
+        inc = 0
+    }else{
+        pls.disabled = false;
+        minus.disabled = false;
+        heart.disabled = false;
+        commint.disabled = false;
+        inc =1
+    }
+})
