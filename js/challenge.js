053a2ecb580de7bd9f40fb9fedf86bda762d8d0c
tugba <ttugbasen@gmail.com>
2020-06-24 16:33:32 +0300

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,39 @@
+let counter = document.getElementById("counter");
+let plus = document.getElementById("plus");
+let minus = document.getElementById("minus");
+let heart = document.getElementById("heart");
+let pause = document.getElementById("pause");
+let likes = document.getElementsByClassName("likes")[0];
+let comments = document.getElementsByClassName("comments")[0];
+let commentInput = document.getElementById("comment-input");
+
+function incrementCounter() {
+    counter.innerText = parseInt(counter.innerText, 10) + 1;
+};
+
+var intervalID = setInterval(incrementCounter, 1000);
+
+
+plus.addEventListener("click", function(e) {
+    incrementCounter();
+});
+minus.addEventListener("click", function(e) {
+    counter.innerText = parseInt(counter.innerText, 10) - 1;
+});
+heart.addEventListener("click", function(e) {
+    let counterValue = counter.innerText;
+    let counterValueElement = document.getElementById(counterValue);
+    if (counterValueElement === null) {
+        let likeTimes = document.createElement("li");
+        likeTimes.innerHTML = `${counterValue} has been liked <span>1</span> time`;
+        likeTimes.id = counterValue;
+        likes.appendChild(likeTimes);
+    } else {
+        let spanElement = counterValueElement.getElementsByTagName("span")[0];
+        let spanValue = parseInt(spanElement.innerText, 10);
+        spanElement.innerText = spanValue + 1;
+        if (spanValue === 1) {
+            counterValueElement.innerHTML += "s";
+        }
+    }  
+});
